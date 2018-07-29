bed664bb168d66122f272194e1f04079df4b039a
Sven <scattell@gmail.com>
2018-07-29 13:10:12 -0700

    made the attack mode portable

--- cifarPatch.py
+++ cifarPatch.py
@@ -1,48 +1,51 @@
-from attacks.targets.cifar10ResNet import CIFAR10ResNet,residual
-from attacks.targets.datasets import CIFAR10
-from attacks.utils import *
-from attacks.patchAttack import *
+from xerxes.targets.cifar10ResNet import CIFAR10ResNet,residual
+from xerxes.targets.datasets import CIFAR10
+from xerxes.utils import *
+from xerxes.patchAttack import *
+from xerxes.patchAttack.dataParallelTrainer import *
 import torch.optim as optim
 from skimage import io, transform, img_as_float
 from skimage.color import gray2rgb
 
 
-model1 = torch.load("cifarnetbn.pickle")
-model2 = torch.load("cifarnetbn.pickle")
+model1 = torch.load("models/cifar10ResNet22.pkl")
+model2 = torch.load("models/cifar10VGG9.pkl")
+model_test = torch.load("models/cifar10VGG12.pkl")
 cifar = CIFAR10()
 batch_size = 400
 loader = cifar.training(batch_size)
 model1.cpu()
 model2.cpu()
 
-mask = np.ones((3,15,15),dtype=np.float32)
-masker = AffineMaskSticker(9,mask,(3,32,32),90,0.6,(0.2,1.2))
-masker.setBatchSize(batch_size)
+mask = np.ones((3,32,32),dtype=np.float32)
 
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam([masker.sticker], lr=0.001)
+sticker = AdversarialSticker(mask,0,(-1,1))
+placer = AffinePlacer(mask,(3,32,32),15,0.6,(0.2,1.0))
+stickerAttack = StickerAttack(sticker,placer,9)
 
 testset = cifar.testing(batch_size)
-untrainedError = masker.test(model1,testset)
+untrainedError = stickerAttack.test(model_test,testset)
 
+criterion = nn.CrossEntropyLoss()
+		
+optimizer = optim.SGD([sticker.sticker], lr= 0.3)
 
-trainPatch(masker,[model1,model2],loader,optimizer,criterion,5,batch_size)
+#untrainedError = stickerAttack.test(model2,loader)
 
-trainedError = masker.test(model,testset)
-print('Untrained success rate: %.5f, Trained success rate: %.5f'%(untrainedError,trainedError))
+stickerTrainer = StickerTrainer(
+		sticker,
+			[model1,
+			model2],
+		[criterion,
+		criterion],
+		9,
+		placer)
+
+stickerTrainer.train(loader,optimizer,1,targetModel = model_test)
 
 dataiter = iter(testset)
 images, labels = dataiter.next()
 images = images.cuda()
-masker.visualize(images,model,filename="sticker_attack.png")
+stickerAttack.visualize(images,model_test,filename="sticker_attack.png")
 
-sticker = (masker.sticker + 1)/2
-sticker = torch.mul(sticker,masker.mask).permute(1,2,0).detach()
-sticker = sticker.cpu().numpy()
-sticker = np.clip(sticker,0,1)
-if sticker.shape[2] == 1:
-	sticker.shape = (15,15)
-	sticker = gray2rgb(sticker)
-
-io.imsave("sticker.png",sticker)
+sticker.save("cifar_sticker.png")
